2c5cbfe9cc5fcca1f143386f193984b2ab48cdba
Talia Bar Zohar <taliabarzohar480@gmail.com>
2024-08-29 17:37:51 +0300

    Create schema and the Category model

---
+++ api/models/category.js
@@ -0,0 +1,12 @@
+//Here we are going to create the schema and the Category model
+
+//Import to mongoose
+const mongoose = require("mongoose");
+
+const categorySchema = mongoose.Schema({
+  title: { type: String, required: true },
+  description: { type: String, required: true },
+});
+
+//Exports to categorySchema that we create
+module.exports = mongoose.model("category", categorySchema);
