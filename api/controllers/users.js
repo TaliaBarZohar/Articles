7a6ec6405789c58e4ab7b005ea2f3d562100b8a5
Talia Bar Zohar <taliabarzohar480@gmail.com>
2024-10-15 00:57:29 +0300

    Create user fod logging

--- api/controllers/users.js
+++ api/controllers/users.js
@@ -1,8 +1,17 @@
+const mongoose = require("mongoose");
+const User = require("../models/user");
+
 module.exports = {
   signup: (req, res) => {
-    res.status(200).json({
-      message: "Signup",
+    const { email, password } = req.body;
+
+    const user = new User({
+      _id: new mongoose.Types.objectId(),
+      email,
+      password,
     });
+
+    user.save();
   },
 
   login: (req, res) => {
