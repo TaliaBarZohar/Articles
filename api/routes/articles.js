3b017d5da19cb146b8f41540391a1ff55af3eda4
Talia Bar Zohar <taliabarzohar480@gmail.com>
2024-10-22 00:33:06 +0300

    Add to router of articles checkAuth function

--- api/routes/articles.js
+++ api/routes/articles.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router(); //Creates a new instance of the Router object
 
 const upload = require("../middlewares/upload");
+const checkAuth = require("../middlewares/checkAuth");
 
 const {
   getAllarticles,
@@ -14,16 +15,16 @@ const {
 //If we want to get article from the web page
 router.get("/", getAllarticles);
 
-//If we want to publish information on the web page
-router.post("/", upload.single("image"), createArticle);
-
 //If we want to get article from the web page y ID
 router.get("/:articleID", getArticle);
 
+//If we want to publish information on the web page
+router.post("/", checkAuth, upload.single("image"), createArticle);
+
 //If we want to update this article
-router.patch("/:articleID", updateArticle);
+router.patch("/:articleID", checkAuth, updateArticle);
 
 //If we want delete something in this article
-router.delete("/:articleID", deleteArticle);
+router.delete("/:articleID", checkAuth, deleteArticle);
 
 module.exports = router;
